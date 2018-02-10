7c0dc15d908ce187d7da41cb5a67ada1d8ef6f62
xuefeng <2803797315@qq.com>
2018-02-10 18:01:55 +0800

    leetcode

--- src/main/java/com/xf/test/leetcode/easy/TrimBinarySearchTree.java
+++ src/main/java/com/xf/test/leetcode/easy/TrimBinarySearchTree.java
@@ -1,6 +1,8 @@
 package com.xf.test.leetcode.easy;
 
 
+import java.util.LinkedList;
+
 /**
  * Given a binary search tree and the lowest and highest boundaries as L and R, trim the tree so that all its elements lies in [L, R] (R >= L). You might need to change the root of the tree, so the result should return the new root of the trimmed binary search tree.
  * <p>
@@ -38,7 +40,7 @@ package com.xf.test.leetcode.easy;
  * 1
  */
 public class TrimBinarySearchTree {
-    public MergeTrees.TreeNode trimBST(MergeTrees.TreeNode root, int l, int r) {
+    public static MergeTrees.TreeNode trimBST(MergeTrees.TreeNode root, int l, int r) {
         if (root == null)
             return root;
         if (root.val < l) {
@@ -52,6 +54,44 @@ public class TrimBinarySearchTree {
 
         return root;
 
+    }
+
+    //前序遍历 递归
+    public static void preFor(MergeTrees.TreeNode treeNode) {
+        if (treeNode == null)
+            return;
+        System.out.println(treeNode.val);
+        preFor(treeNode.left);
+        preFor(treeNode.right);
+    }
+
+    //前序遍历
+    public static void preFor1(MergeTrees.TreeNode treeNode) {
+        if (treeNode == null)
+            return;
+        final LinkedList<MergeTrees.TreeNode> stack = new LinkedList<>();
+        stack.push(treeNode);
+        while (!stack.isEmpty()) {
+            final MergeTrees.TreeNode pop = stack.pop();
+            System.out.println(pop.val);
+            if (pop.right != null)
+                stack.push(pop.right);
+            if (pop.left != null) {
+                stack.push(pop.left);
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        final MergeTrees.TreeNode treeNode = new MergeTrees.TreeNode(3);
+        treeNode.left = new MergeTrees.TreeNode(0);
+        treeNode.right = new MergeTrees.TreeNode(4);
+        treeNode.left.right = new MergeTrees.TreeNode(2);
+        treeNode.left.right.left = new MergeTrees.TreeNode(1);
+        final MergeTrees.TreeNode retNode = trimBST(treeNode, 1, 3);
+        preFor(retNode);
+        preFor1(retNode);
     }
 
 }
